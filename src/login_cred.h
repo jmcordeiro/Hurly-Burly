7312833d94def028f30fe5b67f567beb25f9e76d
Joao Cordeiro <jcordeiro.pt@gmail.com>
2013-05-16 05:21:41 +0100

    Working - with macus students names

--- src/login_cred.h
+++ src/login_cred.h
@@ -12,62 +12,172 @@
 
 using namespace std;
 
-/*
-//-- Pedro ------------------------------------
 
-string hardCodedName_ = "pedro";
-string hardCodedPass_ = "qwertypedro";
-int hardCodedId_ = 3;
-
-
-//-- Johnny ------------------------------------
-string hardCodedName_ = "johnny";
-string hardCodedPass_ = "qwertyjohnny";
-int hardCodedId_ = 5;
-
-//-- Rui ------------------------------------
- 
-string hardCodedName_ = "rui";
-string hardCodedPass_ = "qwertyrui";
-int hardCodedId_ = 2;
-
-//-- Ana ------------------------------------
-string hardCodedName_ = "ana";
-string hardCodedPass_ = "qwertyana";
-int hardCodedId_ = 4;
-
-//-- Peter ------------------------------------
-string hardCodedName_ = "peter";
-string hardCodedPass_ = "qwertypeter";
-int hardCodedId_ = 6;
-
-//-- Ruth ------------------------------------
-string hardCodedName_ = "ruth";
-string hardCodedPass_ = "qwertyruth";
-int hardCodedId_ = 7;
-
-//-- Antony ------------------------------------
-string hardCodedName_ = "antony";
-string hardCodedPass_ = "qwertyantony";
-int hardCodedId_ = 8;
-*/
 /*
  //-- Cordeiro ------------------------------------
  string hardCodedName_ = "joao";
- string hardCodedPass_ = "joao";
- int hardCodedId_ = 11;
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 1;
 */
 /*
 //-- Cris ------------------------------------
 string hardCodedName_ = "cris";
-string hardCodedPass_ = "qwertycris";
-int hardCodedId_ = 10;
+string hardCodedPass_ = "qwerty";
+int hardCodedId_ = 2;
 */
-
+/*
 //-- GabDout ------------------------------------
 string hardCodedName_ = "GabDout";
 string hardCodedPass_ = "qwerty";
 int hardCodedId_ = 3;
+*/
+
+/*
+//-- Dan       ------------------------------------
+string hardCodedName_ = "Dan";
+string hardCodedPass_ = "qwerty";
+int hardCodedId_ = 25;
+*/
+
+
+/*
+ //--  Jugo      ------------------------------------
+ string hardCodedName_ = "Jugo";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 26;
+ */
+
+
+/*
+ //--  Jackie       ------------------------------------
+ string hardCodedName_ = "Jackie ";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 27;
+ */
+
+
+/*
+ //--  Ming      ------------------------------------
+ string hardCodedName_ = "Ming";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 28;
+ */
+
+
+/*
+ //-- Rabee       ------------------------------------
+ string hardCodedName_ = "Rabee";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 29;
+ */
+
+
+/*
+ //-- Carmen       ------------------------------------
+ string hardCodedName_ = "Carmen";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 30;
+ */
+
+
+/*
+ //-- Erik       ------------------------------------
+ string hardCodedName_ = "Erik";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 31;
+ */
+
+/*
+ //--  Andrea      ------------------------------------
+ string hardCodedName_ = "Andrea";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 32;
+ */
+
+/*
+ //--  Lynzy      ------------------------------------
+ string hardCodedName_ = "Lynzy";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 33;
+ */
+
+/*
+ //--  Sandra      ------------------------------------
+ string hardCodedName_ = "Sandra";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 34;
+ */
+
+/*
+ //-- Ronald       ------------------------------------
+ string hardCodedName_ = "Ronald";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 35;
+ */
+
+/*
+ //--  Elly      ------------------------------------
+ string hardCodedName_ = "Elly";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 36;
+ */
+
+/*
+ //-- Miguel       ------------------------------------
+ string hardCodedName_ = "Miguel";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 37;
+ */
+
+/*
+ //--  Amelia      ------------------------------------
+ string hardCodedName_ = "Amelia";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 38;
+ */
+
+/*
+ //-- Justin       ------------------------------------
+ string hardCodedName_ = "Justin";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 39;
+ */
+
+/*
+ //--  Thomas      ------------------------------------
+ string hardCodedName_ = "Thomas";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 40;
+ */
+
+/*
+ //--  Dollar      ------------------------------------
+ string hardCodedName_ = "Dollar";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 41;
+ */
+
+/*
+ //--  Angie      ------------------------------------
+ string hardCodedName_ = "Angie";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 42;
+ */
+
+/*
+ //--  Kafu      ------------------------------------
+ string hardCodedName_ = "Kafu";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 43;
+ */
+
+/*
+ //--  Chloe      ------------------------------------
+ string hardCodedName_ = "Chloe";
+ string hardCodedPass_ = "qwerty";
+ int hardCodedId_ = 44;
+ */
+
 
 
 
